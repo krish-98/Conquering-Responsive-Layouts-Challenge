2974aabaf6324fcc3ca7f857ac3df8293f174521
krish-98 <muralikrishnanm98@gmail.com>
2022-07-29 17:57:09 +0530

    final nav challenge

---
+++ final-challenge/tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        dark: "#2e323f",
+        darkLight: "#3b4050",
+        lightGray: "#6f737f",
+        gold: "#a59678",
+      },
+    },
+  },
+  plugins: [],
+}
